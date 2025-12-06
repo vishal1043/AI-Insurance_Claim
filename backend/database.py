80c60478fab1cd2c491cdaac80cb25737a9af77f
vishal1043 <135422531+vishal1043@users.noreply.github.com>
2025-12-06 16:36:06 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -14,7 +14,7 @@ load_dotenv(ROOT_DIR / ".env")
 # You can also move this to .env as DATABASE_URL
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+psycopg://postgres:vishal@localhost:5432/insurance_db",
+    
 )
 
 if not DATABASE_URL:
